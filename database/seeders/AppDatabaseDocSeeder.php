b89afe62d405d24628fef593ae937768bc4821a8
Abdu Rangga Senjari <abduranggasenjari@gmail.com>
2026-07-07 23:22:55 +0700

    fix: use Str::slug in AppDatabaseDocSeeder

--- database/seeders/AppDatabaseDocSeeder.php
+++ database/seeders/AppDatabaseDocSeeder.php
@@ -5,20 +5,22 @@ namespace Database\Seeders;
 use App\Models\Aplikasi;
 use App\Models\AppDatabaseDoc;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class AppDatabaseDocSeeder extends Seeder
 {
     public function run(): void
     {
         Aplikasi::each(function (Aplikasi $aplikasi) {
+            $dbName = 'db_' . Str::slug($aplikasi->nama, '_');
             AppDatabaseDoc::create([
                 'aplikasi_id' => $aplikasi->id,
-                'nama_database' => 'db_' . str($aplikasi->nama)->slug('_'),
+                'nama_database' => $dbName,
                 'tipe_dbms' => collect(['MySQL', 'PostgreSQL', 'MariaDB', 'MongoDB'])->random(),
                 'versi' => collect(['8.0', '14', '10.11', '7.0'])->random(),
                 'host' => 'localhost',
                 'port' => collect([3306, 5432, 3307, 27017])->random(),
-                'nama_db_asli' => 'db_' . str($aplikasi->nama)->slug('_'),
+                'nama_db_asli' => $dbName,
                 'jumlah_tabel' => rand(5, 50),
                 'keterangan' => 'Database untuk ' . $aplikasi->nama,
             ]);
